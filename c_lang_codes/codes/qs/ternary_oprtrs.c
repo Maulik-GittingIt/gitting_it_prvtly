397ce64877c5a8719e8b21de8596c60b305d39aa
Maulik Bahl <maulikbahl@gmail.com>
2024-12-04 12:55:17 +0530

    ok
    changing location and all

--- c_lang_codes/codes/qs/ternary_oprtrs.c
+++ c_lang_codes/codes/qs/ternary_oprtrs.c
@@ -6,6 +6,6 @@ int main(){
     int a = 123;
     int b = 1234;
     a>b? printf("a is greater than b") : printf("b is greater than a");
-    printf("done");
+    printf("done 2:");
     return 0;
 }
