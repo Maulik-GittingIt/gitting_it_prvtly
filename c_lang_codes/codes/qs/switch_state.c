7290ca2dad1be0e095d08a73e1bfb09de4a9a39c
Maulik Bahl <maulikbahl@gmail.com>
2024-12-04 13:21:18 +0530

    switch statement 1

---
+++ c_lang_codes/codes/qs/switch_state.c
@@ -0,0 +1,23 @@
+// using switch statement 
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("enter the number:- ");
+    scanf("%d", &a);
+    switch(a){
+        case 1 :
+            printf("u entered 1");
+            break;
+        case 2 :
+            printf("u entered 2");
+            break;
+        case 3 :
+            printf("u entered 2");
+            break;
+        case 4 :
+            printf("u entered 2");
+            break;
+    }
+    return 0;
+}
