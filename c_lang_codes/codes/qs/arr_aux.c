ecf37a8be0f01537495124f8a51899339cba00d2
Maulik Bahl <maulikbahl@gmail.com>
2025-01-19 23:35:52 +0530

    com

--- c_lang_codes/codes/qs/arr_aux.c
+++ c_lang_codes/codes/qs/arr_aux.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    
+    int arr[5]={1,2,3,4,5};
+    int aux[5];
+    for (int i =0;i<5;i++){
+        aux[4-i]=arr[i];
+    }
+    for(int i=0;i<5;i++){
+        printf("%d\t",aux[i]);
+    }
     return 0;
 }
