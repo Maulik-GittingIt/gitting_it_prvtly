c234f0a2f15e54d7009c0be61d46fb85a56443c3
Maulik Bahl <maulikbahl@gmail.com>
2025-07-15 12:25:53 +0530

    krta hu commit

---
+++ python codes/dictonary_basics.py
@@ -0,0 +1,2 @@
+marks = {"maulik":100, "abs":99, "bbbb":88}
+print(marks["abs"])#instead of traditionally writing index like for list, string or tuple we can simply write the key to extract the value in a dictionary
